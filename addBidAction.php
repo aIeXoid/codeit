f65c9c0682acd7e7721035f81b6f4af6ef8e427f
aleXoid <alexoidsed@gmail.com>
2023-12-11 20:04:47 +0100

    changed max bid logic

--- addBidAction.php
+++ addBidAction.php
@@ -5,7 +5,7 @@ class AddBidAction{
     public function __construct(private PDO $db) {
         //
     }
-    
+
     public function addBid($data): void {
         
         $user_id = rand(1,1000); //get authorized user_id 
@@ -19,23 +19,33 @@ class AddBidAction{
             //transaction to get current bid data and save new bid
             $this->db->beginTransaction();
 
-            $currentBidStmt = $this->db->prepare("SELECT `bid` FROM `bid` WHERE `auction_id` = :auction_id ORDER BY `bid` DESC LIMIT 1");
+            //Lock max bid for transaction
+            $currentBidStmt = $this->db->prepare("SELECT `bid` FROM `auction` WHERE `id` = :auction_id FOR UPDATE");
             $currentBidStmt->bindParam(':auction_id', $auction_id, PDO::PARAM_INT);
             $currentBidStmt->execute();
             $currentBid = $currentBidStmt->fetchColumn();
-
+           
             //Check for conditions
             if (($bid - $currentBid) < 1 && $bid <= $currentBid) {
                 throw new Exception("The bet cannot be less than 1 dollar and less than the current one");
             }
 
+            //sleep for test
+            sleep(rand(10,30));
+            
             //Save new bid if all ok
-            $newBidStmt = $this->db->prepare("INSERT INTO bid (auction_id, user_id, bid) VALUES (:auction_id, :user_id, :new_bid)");
+            $newBidStmt = $this->db->prepare("INSERT INTO `bid` (`auction_id`, `user_id`, `bid`) VALUES (:auction_id, :user_id, :new_bid)");
             $newBidStmt->bindParam(':auction_id', $auction_id, PDO::PARAM_INT);
             $newBidStmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
             $newBidStmt->bindParam(':new_bid', $bid, PDO::PARAM_STR);
             $newBidStmt->execute();
 
+            //update max bid
+            $newBidStmt = $this->db->prepare("UPDATE `auction` SET `bid` = :new_bid, `winner` = :user_id WHERE `id` = :auction_id");
+            $newBidStmt->bindParam(':auction_id', $auction_id, PDO::PARAM_INT);
+            $newBidStmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+            $newBidStmt->bindParam(':new_bid', $bid, PDO::PARAM_STR);
+            $newBidStmt->execute();
             
             $this->db->commit();
             
